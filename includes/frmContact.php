ce5ca9eb8011274017d834f998abf6f272d93091
Arthur CLARKE <arthurclarke312@gmail.com>
2019-12-13 15:19:01 +0100

    second commit

---
+++ includes/frmContact.php
@@ -0,0 +1,25 @@
+<form action="index.php?page=contact" method="post">
+  <fieldset>
+    <legend id="legend">Me contacter</legend>
+  <div>
+    <label for="nom">Nom :</label>
+    <input type="text" name="nom" id="nom" />
+  </div>
+  <div>
+    <label for="prenom">Prénom :</label>
+    <input type="text" name="prenom" id="prenom" />
+  </div>
+  <div>
+    <label for="mail">e-mail :</label>
+    <input type="mail" name="mail" id="mail" />
+  </div>
+  <div>
+    <label for="msg">Message : </label>
+  <textarea name="msg" id="msg"></textarea>
+  </div>
+  <div>
+<input type="submit" value="Send" id="case5" />
+  </div>
+  </fieldset>
+  <input type="hidden" name="frmContact" />
+</form>
---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<h1>Arthur CLARKE | Référent Digital</h1>
+  <nav id="menu">
+    <ul>
+      <li><a href="index.php?page=accueil" title="Retour à l'accueil">Accueil</a></li>
+      <li><a href="index.php?page=contact" title="Me contacter">Contact</a></li>
+      <li><a href="index.php?page=cv" title="Voir mon CV">CV</a></li>
+    </ul>
+  </nav>
