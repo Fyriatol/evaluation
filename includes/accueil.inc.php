e72d93274b67ca6a44d0bbf00224ce5f1f7e4398
Arthur CLARKE <arthurclarke312@gmail.com>
2019-12-14 15:24:02 +0100

    wesh

--- includes/accueil.inc.php
+++ includes/accueil.inc.php
@@ -1,22 +1,24 @@
-<h1>ACCUEIL</H1>
-<div id="qui">
-  <h2 id="prez">QUI SUIS-JE ?</h2>
-    <p id="qui">Je me présente: Arthur CLARKE.</p>
-    <p id="qui">Comme le dirais si bien un de mes camarades, je m'appelle 22 ans. Suite à une fac d'Anglais assez catastrophique, je me suis tourné vers la formation de Référent Digital, car je suis un passionné de l'univers du numérique et du digital. J'aime également les pâtes, mais je ne pense pas que cela ai un quelconque rapport.
+<div id="header">
+<h1 id="accueil">ACCUEIL</h1>
 </div>
-<div id="prez">
-  <h2 id="prez">Ma Formation</h2>
-    <p id="formation">Je suis actuellement en formation de <a href="https://nfactory.school/formation-referent-digital/" target="_blank">référent digital</a> à la <a href="https://nfactory.school/" target="_blank">NFactory School</a></p>
-    <p id="formation">Cette formation a pour but de nous introduire à tout l'univers du Digital via divers enseignement tel que :
-      <div class="band">
-        <ul>
-          <div class="wrap">
-            <li>Code (HTML, CSS, PHP, ...)</li>
-            <li>PAO (Photoshop, Illustrator,...)</li>
-            <li>Webmarketing</li>
-            <li>etc... </li>
-            <li>En gros, tout ce qui sert à être présent sur les internets du web</li></p>
-          </div>
-        </ul>
+  <div id="qui">
+    <h2 id="prez">QUI SUIS-JE ?</h2>
+      <p id="qui">Je me présente: Arthur CLARKE.</p>
+      <p id="qui">Comme le dirais si bien un de mes camarades, je m'appelle 22 ans. Suite à une fac d'Anglais assez catastrophique, je me suis tourné vers la formation de Référent Digital, car je suis un passionné de l'univers du numérique et du digital. J'aime également les pâtes, mais je ne pense pas que cela ai un quelconque rapport.
+  </div>
+  <div id="prez">
+    <h2 id="prez">Ma Formation</h2>
+      <p id="formation">Je suis actuellement en formation de <a href="https://nfactory.school/formation-referent-digital/" target="_blank">référent digital</a> à la <a href="https://nfactory.school/" target="_blank">NFactory School</a></p>
+      <p id="formation">Cette formation a pour but de nous introduire à tout l'univers du Digital via divers enseignement tel que :
+  <div class="band">
+    <ul>
+      <div class="wrap">
+        <li>Code (HTML, CSS, PHP, ...)</li>
+        <li>PAO (Photoshop, Illustrator,...)</li>
+        <li>Webmarketing</li>
+        <li>etc... </li>
+        <li>En gros, tout ce qui sert à être présent sur les internets du web</li></p>
       </div>
+    </ul>
+  </div>
 </div>
--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,10 @@
+<div id="header">
 <h1>Arthur CLARKE | Référent Digital</h1>
+</div>
   <nav id="menu">
-    <ul>
-      <li id="header"><a href="index.php?page=accueil" title="Retour à l'accueil">Accueil</a></li>
-      <li id="header"><a href="index.php?page=contact" title="Me contacter">Contact</a></li>
-      <li id="header"><a href="index.php?page=cv" title="Voir mon CV">CV</a></li>
+    <ul id="headeur">
+      <li id="headeur"><a href="index.php?page=accueil" title="Retour à l'accueil">Accueil</a></li>
+      <li id="headeur"><a href="index.php?page=contact" title="Me contacter">Contact</a></li>
+      <li id="headeur"><a href="index.php?page=cv" title="Voir mon CV">CV</a></li>
     </ul>
   </nav>
