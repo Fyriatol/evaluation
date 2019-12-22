a8d4defb3f6e5cc3c4caa74372252ecaea21649b
Arthur CLARKE <arthurclarke312@gmail.com>
2019-12-22 15:55:05 +0100

    Changement titre

--- includes/html.php
+++ includes/html.php
@@ -2,7 +2,7 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Mon premier site tout seul</title>
+    <title>Site Arthur | Évaluation</title>
      <link rel="stylesheet" href="./assets/css/styles.css">
     <title>SITE ARTHUR</title>
   </head>
