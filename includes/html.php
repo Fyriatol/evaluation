3c155c0eb540eb86e4761675b99fa8b327ffa23f
Arthur CLARKE <arthurclarke312@gmail.com>
2020-01-10 16:55:03 +0100

    tentative de responsive ratée

--- includes/html.php
+++ includes/html.php
@@ -2,7 +2,8 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Site Arthur | Évaluation</title>
+    <meta name="viewport" content="width=device-width, target-densitydpi=device-dpi"/>
+      <title>Site Arthur | Évaluation</title>
      <link rel="stylesheet" href="./assets/css/styles.css">
     <title>SITE ARTHUR</title>
   </head>
